2fd83d6402696d247d39eddd725c60874629585f
Bruce <bruce.z@infinitytesting.com.au>
2018-12-13 16:52:39 +0800

    ^ build index module

---
+++ config/backend/menu.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Copyright © 2018 CrazyCat, Inc. All rights reserved.
+ * See COPYRIGHT.txt for license details.
+ */
+
+/**
+ * @category CrazyCat
+ * @package CrazyCat\Index
+ * @author Bruce Z <152416319@qq.com>
+ * @link http://crazy-cat.co
+ */
+return [
+    'config' => [
+        'label' => __( 'Configuration' ),
+        'sort_order' => 998
+    ]
+];
