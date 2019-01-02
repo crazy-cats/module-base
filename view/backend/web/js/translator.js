164828ac88114d3db90e4bebf6d739ea48d6e096
Bruce <bruce.z@infinitytesting.com.au>
2019-01-02 18:15:14 +0800

    ^ build core module

--- view/backend/web/js/translator.js
+++ view/backend/web/js/translator.js
@@ -4,10 +4,14 @@
  */
 define( [ 'jquery' ], function( $ ) {
 
-    return function( string ) {
-
-        return string;
-
+    return function( string, params ) {
+        params = params || [ ];
+        var translations = {};
+        var translatedText = translations[string] || string;
+        for ( var k = 0; k < params.length; k++ ) {
+            translatedText = translatedText.replace( '%' + (k + 1), params[k] );
+        }
+        return translatedText;
     };
 
 } );
