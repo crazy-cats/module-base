0d1f63c2bb66f3847524d1802f4f5afa0f3116f0
zengliwei <zengliwei@163.com>
2020-06-11 10:50:29 +0800

    ^ change default value getting function of scope config

--- config/settings.php
+++ config/settings.php
@@ -7,22 +7,25 @@ return [
         'sort_order' => 1,
         'fields'     => [
             'timezone'          => [
-                'label'  => 'Timezone',
-                'scopes' => ['global'],
-                'type'   => 'select',
-                'source' => 'CrazyCat\Base\Model\Source\TimeZones'
+                'label'   => 'Timezone',
+                'scopes'  => ['global'],
+                'type'    => 'select',
+                'source'  => 'CrazyCat\Base\Model\Source\TimeZones',
+                'default' => 'Asia/Shanghai'
             ],
             'allowed_languages' => [
-                'label'  => 'Allowed Languages',
-                'scopes' => ['global', 'stage'],
-                'type'   => 'multiselect',
-                'source' => 'CrazyCat\Base\Model\Source\Languages'
+                'label'   => 'Allowed Languages',
+                'scopes'  => ['global', 'stage'],
+                'type'    => 'multiselect',
+                'source'  => 'CrazyCat\Base\Model\Source\Languages',
+                'default' => ['en_US']
             ],
             'default_language'  => [
-                'label'  => 'Default Languages',
-                'scopes' => ['global', 'stage'],
-                'type'   => 'select',
-                'source' => 'CrazyCat\Base\Model\Source\Languages'
+                'label'   => 'Default Languages',
+                'scopes'  => ['global', 'stage'],
+                'type'    => 'select',
+                'source'  => 'CrazyCat\Base\Model\Source\Languages',
+                'default' => 'en_US'
             ]
         ]
     ],
@@ -39,25 +42,29 @@ return [
                 'default' => 'default'
             ],
             'default_page_title'       => [
-                'label'  => 'Default Page Title',
-                'scopes' => ['global', 'stage'],
-                'type'   => 'text'
+                'label'   => 'Default Page Title',
+                'scopes'  => ['global', 'stage'],
+                'type'    => 'text',
+                'default' => 'CrazyCat'
             ],
             'default_meta_keywords'    => [
-                'label'  => 'Default Meta Keywords',
-                'scopes' => ['global', 'stage'],
-                'type'   => 'text'
+                'label'   => 'Default Meta Keywords',
+                'scopes'  => ['global', 'stage'],
+                'type'    => 'text',
+                'default' => 'PHP, framework, CMF, CMS, composer, CrazyCat'
             ],
             'default_meta_description' => [
-                'label'  => 'Default Meta Description',
-                'scopes' => ['global', 'stage'],
-                'type'   => 'textarea'
+                'label'   => 'Default Meta Description',
+                'scopes'  => ['global', 'stage'],
+                'type'    => 'textarea',
+                'default' => 'This is a PHP content management framework based on composer.'
             ],
             'default_meta_robots'      => [
-                'label'  => 'Default Robots',
-                'scopes' => ['global', 'stage'],
-                'type'   => 'select',
-                'source' => 'CrazyCat\Base\Model\Source\MetaRobots'
+                'label'   => 'Default Robots',
+                'scopes'  => ['global', 'stage'],
+                'type'    => 'select',
+                'source'  => 'CrazyCat\Base\Model\Source\MetaRobots',
+                'default' => 'INDEX, FOLLOW'
             ]
         ]
     ]
